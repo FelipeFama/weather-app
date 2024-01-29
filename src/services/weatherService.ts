441711e1ae4fa4d8d845bb03c06d87cfc269bcc7
FelipeFama <lipehfama@gmail.com>
2024-01-29 19:52:22 -0300

    feat: add weatherservice

---
+++ src/services/weatherService.ts
@@ -0,0 +1,26 @@
+import axios from "axios";
+
+const apiKey = import.meta.env.VITE_KEY_WEATHER;
+
+export const fetchWeather = async (city: string) => {
+  try {
+    const response = await axios.get(
+      `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`
+    );
+    return response.data;
+    console.log(response)
+  } catch (error) {
+    console.error("Error fetching weather data:", error);
+    throw error;
+  }
+};
+
+export const fetchWeatherByIP = async () => {
+  try {
+    const response = await axios.get("https://ipinfo.io/json");
+    return response.data.city;
+  } catch (error) {
+    console.error("Error fetching IP data:", error);
+    throw error;
+  }
+};
