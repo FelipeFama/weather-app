5bf3d6505fe205e876935894ad784008273f77dc
FelipeFama <lipehfama@gmail.com>
2024-01-29 19:05:34 -0300

    feat: add new styles and informations in weatherinfo component

--- src/components/WeatherInfo/index.tsx
+++ src/components/WeatherInfo/index.tsx
@@ -1,9 +1,22 @@
 import React from "react";
+import { Weather } from "./styles";
 
 export const WeatherInfo = () => {
   return (
     <>
-      <article></article>
+      <Weather>
+        <h2>Weather in Fortaleza</h2>
+        <h1>28ºC</h1>
+        <section>
+          <img
+            src="https://openweathermap.org/img/wn/01d.png"
+            alt="weather icon"
+          />
+          <p>Clear sky</p>
+        </section>
+        <p>humidity: 40%</p>
+        <p>Wind a speed: 6.2 km/h</p>
+      </Weather>
     </>
   );
 };
---
+++ src/components/WeatherInfo/styles.ts
@@ -0,0 +1,5 @@
+import styled from "styled-components";
+
+export const Weather = styled.article`
+  margin-left: 1rem;
+`;
