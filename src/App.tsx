92a2640a328ae8e9f7f1b86858d2e4020b387d13
FelipeFama <lipehfama@gmail.com>
2024-01-29 22:11:31 -0300

    feat: add return fetch and types

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,22 @@
+import { useEffect } from "react";
 import { SearchBar } from "./components/SearchBar";
 import { WeatherInfo } from "./components/WeatherInfo";
 import { GlobalStyles } from "./styles/global";
 import { WeatherCard } from "./styles/styles";
+import { fetchWeather } from "./services/weatherService";
 
 export default function App() {
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const data = await fetchWeather("fortaleza");
+        console.log(data);
+      } catch (error) {
+        console.log("Error fetching weather data:", error);
+      }
+    };
+    fetchData();
+  }, []);
   return (
     <>
       <GlobalStyles />
---
+++ src/components/types/index.ts
@@ -0,0 +1,14 @@
+export interface WeatherData {
+    name: string;
+    main: {
+      temp: number;
+      humidity: number;
+    };
+    weather: {
+      description: string;
+      icon: string;
+    }[];
+    wind: {
+      speed: number;
+    };
+  }
